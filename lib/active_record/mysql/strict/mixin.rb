2bc330dcbce372be306ee0344d79f65a19f83420
Rémi Prévost <remi@exomel.com>
2013-08-05 14:21:47 -0400

    Add basic functionality

--- lib/active_record/mysql/strict/mixin.rb
+++ lib/active_record/mysql/strict/mixin.rb
@@ -1,5 +1,36 @@
 module ActiveRecord
-  module Mixin
-    # The magic will happen here
+  module MySQL
+    module Strict
+      module Mixin
+        extend ActiveSupport::Concern
+
+        included do
+          define_mysql_strict_validations
+        end
+
+        module ClassMethods
+          def define_mysql_strict_validations
+            except = @mysql_strict_options[:except] || []
+            model_columns = self.columns.dup.reject { |c| except.include?(c.name.to_sym) }
+
+            if only = @mysql_strict_options[:only]
+              model_columns = model_columns.select { |c| only.include?(c.name.to_sym) }
+            end
+
+            model_columns.select { |c| c.type == :string }.map(&:name).each do |field|
+              validates field, length: { in: 0..255 }, allow_blank: true
+            end
+
+            model_columns.select { |c| c.type == :text }.map(&:name).each do |field|
+              validates field, length: { in: 0..65535 }, allow_blank: true
+            end
+
+            model_columns.select { |c| c.type == :integer }.map(&:name).each do |field|
+              validates field, numericality: { greather_than_or_equal_to: -2147483647, less_than_or_equal_to: 2147483647 }, allow_blank: true
+            end
+          end
+        end
+      end
+    end
   end
 end
---
+++ lib/activerecord_mysql_strict.rb
@@ -0,0 +1,13 @@
+require 'active_record/mysql/strict/version'
+
+require 'active_record'
+require 'active_support'
+
+require 'active_record/mysql/strict/mixin'
+
+class ActiveRecord::Base
+  def self.validates_strict_columns(options = {})
+    @mysql_strict_options = options
+    include ActiveRecord::MySQL::Strict::Mixin
+  end
+end
